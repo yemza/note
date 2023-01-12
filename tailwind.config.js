d199bb56c707a8438af4288cbb15eea2c0aa06ee
yemza <elbouazzaoui729@gmail.com>
2023-01-12 10:08:39 +0100

    Add the note pad and menu note

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
-  theme: {
-    extend: {},
+  mode: 'jit',
+  content: {
+    enabled: true,
+    // enabled: process.env.TAILWIND_MODE === 'build',
+    content: ['./src/**/*.{html,ts}']
   },
+  darkMode: 'media', // or 'media' or 'class'
+  theme: {
+    container: {
+      padding: '2rem',
+    },
+    fontFamily: {
+      'sans': ['Open Sans', 'ui-sans-serif', 'system-ui'],
+    }
+  },
+  variants: {},
   plugins: [],
-}
+};
